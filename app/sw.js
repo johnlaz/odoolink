b4c181253f0f70318af6647adf9ed6908ae44ca7
johnlaz <51280623+johnlaz@users.noreply.github.com>
2026-08-08 23:08:17 -0400

    Update service worker cache name to v15

--- app/sw.js
+++ app/sw.js
@@ -1,6 +1,6 @@
 // OdooLink App Service Worker
 // Scope: /odoolink/app/
-const CACHE_NAME = 'odoolink-app-v14';
+const CACHE_NAME = 'odoolink-app-v15';
 const STATIC_ASSETS = [
   '/odoolink/app/',
   '/odoolink/app/index.html',
